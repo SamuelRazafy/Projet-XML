82e4ed23c72be4c16cff9cdd84d4a5a57f08553b
Sam <salamaikoto@gmail.com>
2022-07-23 11:07:53 +0300

    Projet Asp net

---
+++ TPMvcEva/Controllers/FirstController.cs
@@ -0,0 +1,60 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using TPMvcEva.Models;
+
+namespace TPMvcEva.Controllers
+{
+    public class FirstController : Controller
+    {
+        MySqlConnection conn = new MySqlConnection();
+
+        MySqlCommand comm = new MySqlCommand();
+        
+        // GET: First
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public void ClassConn()
+        {
+            conn.ConnectionString = "datasource=localhost;port=3308;database=rh;username=root;password=";
+        }
+
+       [HttpPost]
+
+              public ContentResult Verifier(Info po)
+               {
+                   ClassConn();
+                   conn.Open();
+                   String query = "SELECT * FROM `demandeconge` WHERE  `N° matricule`='" + po.matricule + "'";
+                   MySqlCommand cmd = new MySqlCommand(query, conn);
+                   MySqlDataReader reader = cmd.ExecuteReader();
+                   if (reader.Read())
+                   {
+                       po.departement = reader.GetString("Departement");
+                       po.service = reader.GetString("Service");
+                      string text = string.Format("departement={0}, service={0}", po.departement, po.service);
+                      return Content(text, "text/plain", Encoding.UTF8);
+            /*     return View("Ato", po);*/
+                       
+                   }
+                   else
+                   {
+                       po.departement = string.Empty;
+                       po.service = string.Empty;
+                     string text = string.Format("departement={0}, service={0}", po.departement, po.service);
+                     return Content(text, "text/plain", Encoding.UTF8);
+                  //       return View("Ato", po);
+            }
+                 
+              
+        }
+
+    }
+}
---
+++ TPMvcEva/Models/Info.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TPMvcEva.Models
+{
+    public class Info
+    { 
+        [Required(ErrorMessage ="Champs obligatoire")]
+        public string nom { get; set; }
+        [Required]
+        public string prenom { get; set; }
+        [Required]
+        public string matricule { get; set; }
+        [Required]
+        public string fonction { get; set; }
+        public string departement { get; set; }
+        public string service { get; set; }
+    }
+}
